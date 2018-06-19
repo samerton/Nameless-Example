b5fca4de7701dba2b9e3df4b2a18cc34f312be54
Eftimie Andrei <bax@baxandrei.ro>
2018-06-19 17:29:02 +0300

    Create example.php

---
+++ modules/Example/languages/Romanian/example.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  Romanian Language for example module
+ *  Translation By @BaxAndrei ( https://baxandrei.ro )
+ *  Last Update: 19/06/2018
+ */
+ 
+$language = array(
+	'example' => 'Exemplu'
+);
